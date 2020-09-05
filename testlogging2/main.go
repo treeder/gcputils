552c86f74a080b503576f8a5c347274d9f122dd9
Travis Reeder <treeder@gmail.com>
2020-09-05 10:59:49 -0700

    Properly log gotils Errors

---
+++ testlogging2/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"context"
+
+	"github.com/treeder/gcputils"
+	"github.com/treeder/gotils"
+)
+
+func main() {
+	ctx := context.Background()
+	err := yo(ctx)
+	gcputils.Error().Printf("dang: %v", err)
+}
+
+func yo(ctx context.Context) error {
+	ctx = gotils.With(ctx, "yo", "dawg")
+	return gotils.C(ctx).Errorf("error1")
+}
